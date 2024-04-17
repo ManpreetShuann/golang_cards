f8e408579d229cdfe64eeb76bbd0be5bbf1582c9
Manpreet Shuann <manpreet.shuann@ellucian.com>
2024-04-17 23:59:12 +0530

    feat: Added a reciever function print

--- deck.go
+++ deck.go
@@ -1,4 +1,13 @@
 package main
 
+import "fmt"
+
 // Create a new type of deck which is a slice of strings
 type deck []string
+
+// reciever function: We added a reciever(d deck), which means any variable of type deck now gets access to print method
+func (d deck) print() {
+	for _, card := range d {
+		fmt.Println(card)
+	}
+}
--- main.go
+++ main.go
@@ -1,12 +1,8 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := deck{"Ace of Spades", newcard()}
-	for _, card := range cards {
-		fmt.Println(card)
-	}
+	cards := deck{"Ace of Spades", newcard()} // This is equivalent to []string{}
+	cards.print()
 }
 
 // go wants us to always add what type of values a function would return
