a6f33422b5e3a2e6c12ec5c96216830529b426c4
Manpreet Shuann <manpreet.shuann@ellucian.com>
2024-04-22 23:59:40 +0530

    feat: better tests :)

--- deck_test.go
+++ deck_test.go
@@ -1,10 +1,43 @@
 package main
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	cards := newDeck()
 	if len(cards) != 52 {
 		t.Errorf("Expected 52 cards, got %d", len(cards))
+	}
+	spades := 0
+	clubs := 0
+	hearts := 0
+	diamonds := 0
+	for _, card := range cards {
+		if strings.Contains(card, "Spades") {
+			spades += 1
+		}
+		if strings.Contains(card, "Clubs") {
+			clubs += 1
+		}
+		if strings.Contains(card, "Clubs") {
+			hearts += 1
+		}
+		if strings.Contains(card, "Clubs") {
+			diamonds += 1
+		}
+	}
+	if spades != 13 {
+		t.Errorf("Expected 13 Spades, got %d", spades)
+	}
+	if clubs != 13 {
+		t.Errorf("Expected 13 Clubs, got %d", clubs)
+	}
+	if hearts != 13 {
+		t.Errorf("Expected 13 Hearts, got %d", hearts)
+	}
+	if diamonds != 13 {
+		t.Errorf("Expected 13 Diamonds, got %d", diamonds)
 	}
 }
